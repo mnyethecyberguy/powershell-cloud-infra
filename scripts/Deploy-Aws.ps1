af462c930496b54b7e2d8b92231b7684e9296136
Michael Nye <mnye11@icloud.com>
2023-09-09 15:17:42 -0400

    Updated AWS deployment scripts

--- scripts/Deploy-Aws.ps1
+++ scripts/Deploy-Aws.ps1
@@ -1,3 +1,31 @@
-Write-Host "Deploying AWS Infrastructure************"
+
+
+$ModuleDir = "$(Split-Path $PSScriptRoot)/modules"
+Import-Module $ModuleDir/PwshCloudInfrastructure.psm1 -Force
+
+$CallingDir = "$PWD"
+
+$Regions = "$(Get-Regions)"
+$UniqueStringAws = "$(Get-UniqueStringAws)"
+$DefaultRegionAws = "$(Get-RegionAws)"
+
+if ( ($Regions -eq "null") -or ($UniqueStringAws -eq "null") -or ($DefaultRegionAws -eq "none")) {
+  Write-Host "AWS not configured.  Please run Deploy-Infrastructure.ps1 to enable AWS."
+  exit 1
+}
+
 Write-Host ""
-Write-Error -Message "Deploy-Aws.ps1 -- Test Error message redirection to log."
+Write-Host "Deploying AWS infrastructure....."
+Write-Host ""
+
+$TF_VAR_UniqueStringAws = "$UniqueStringAws"
+$TF_VAR_DeploymentFile = "$(Get-DeployFilePath)"
+
+Set-Location "$(Split-Path -Path $PSScriptRoot)/infrastructure/terraform/aws"
+
+terraform init -backend-config="bucket=tf-$UniqueStringAws"
+terraform apply -auto-approve -lock=false
+
+Write-Host ""
+
+Set-Location $CallingDir
--- scripts/Initialize-TfstateAws.ps1
+++ scripts/Initialize-TfstateAws.ps1
@@ -8,7 +8,8 @@ $UniqueStringAws = "$(Get-UniqueStringAws)"
 $DefaultRegionAws = "$(Get-RegionAws)"
 
 if ( ($Regions -eq "null") -or ($UniqueStringAws -eq "null") -or ($DefaultRegionAws -eq "none")) {
-  Write-Host "AWS not configured.  Please run Deploy-Infrastructure.ps1"
+  Write-Host "AWS not configured.  Please run Deploy-Infrastructure.ps1 to enable AWS."
+  exit 1
 }
 
 Write-Host ""
@@ -18,9 +19,9 @@ Write-Host ""
 
 Write-Host "Checking for AWS Terraform state bucket..."
 $BucketName = "tf-$UniqueStringAws"
-#$BucketExists = "$(aws s3api list-buckets --query "Buckets[?Name == $BucketName]" | jq '. | length')"
+$BucketExists = "$(aws s3api list-buckets --query "Buckets[?Name == ``$BucketName``]" | jq '. | length')"
 
-if ( $(aws s3api list-buckets --query "Buckets[].Name" | jq -r '.[] | select(startswith("tf"))') -eq "" ) {
+if ( $BucketExists -eq 0 ) {
   Write-Host "AWS Terraform state bucket $BucketName does not exist. Creating now..."
   aws s3api create-bucket --bucket $BucketName --region $DefaultRegionAws --create-bucket-configuration LocationConstraint=$DefaultRegionAws
   #aws s3api put-bucket-versioning --bucket $BucketName --versioning-configuration Status=Enabled
