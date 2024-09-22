4d47ba9fba63eca098341994706619fafd987ab6
Michael Nye <mnye11@icloud.com>
2024-09-22 19:00:10 -0400

    Updating Confirm-ResourcesAws.ps1

--- scripts/Confirm-ResourcesAws.ps1
+++ scripts/Confirm-ResourcesAws.ps1
@@ -1,3 +1,29 @@
 Write-Host ""
 Write-Host "Confirming AWS resources are removed....."
 Write-Host ""
+
+$ModuleDir = "$(Split-Path $PSScriptRoot)/modules"
+Import-Module $ModuleDir/PwshCloudInfrastructure.psm1 -Force
+
+$CallingDir = "$PWD"
+$ScriptDir = "$($(Set-Location $PSScriptRoot) > $null && $PWD)"
+
+$DeployFile = $(Get-DeployFilePath)
+$UniqueStringAws = "$(Get-UniqueStringAws)"
+
+<# Check if CloudTrail exists #>
+if ( $(aws cloudtrail list-trails | jq '.Trails[] | select(.name=="pwsh-cloud-trail").name') -ne "" ) {
+  Write-Host "CloudTrail still exists."
+}
+
+<# Check if tfstate bucket exists #>
+if ( $(aws s3api list-buckets --query "Buckets[?Name == ``pwsh-tfstate-$UniqueStringAws``]" | jq '. | length') -ne 0 ) {
+  Write-Host "tfstate bucket still exists."
+}
+
+<# Check if tfstate bucket exists #>
+if ( $(aws s3api list-buckets --query "Buckets[].Name" | jq -r '.[] | select(startswith("pwsh-tfstate"))') -ne 0 ) {
+    Write-Host "tfstate bucket still exists."
+}
+
+<# Check if EC2 instance exists #>
