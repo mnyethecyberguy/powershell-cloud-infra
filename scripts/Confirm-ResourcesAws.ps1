c724d277d88e91429c42818e475d9a60666eb7d0
Michael Nye <mnye11@icloud.com>
2024-09-23 20:05:40 -0400

    Updating Confirm-ResourcesAws.ps1

--- scripts/Confirm-ResourcesAws.ps1
+++ scripts/Confirm-ResourcesAws.ps1
@@ -5,28 +5,28 @@ Write-Host ""
 $ModuleDir = "$(Split-Path $PSScriptRoot)/modules"
 Import-Module $ModuleDir/PwshCloudInfrastructure.psm1 -Force
 
-$CallingDir = "$PWD"
-$ScriptDir = "$($(Set-Location $PSScriptRoot) > $null && $PWD)"
+#$CallingDir = "$PWD"
+#$ScriptDir = "$($(Set-Location $PSScriptRoot) > $null && $PWD)"
 
-$DeployFile = $(Get-DeployFilePath)
-$UniqueStringAws = "$(Get-UniqueStringAws)"
+#$DeployFile = $(Get-DeployFilePath)
+#$UniqueStringAws = "$(Get-UniqueStringAws)"
 
 <# Check if CloudTrail exists #>
-if ( $(aws cloudtrail list-trails | jq '.Trails[] | select(.name=="pwsh-cloud-trail").name') -ne $null ) {
+if (  $null -ne $(aws cloudtrail list-trails | jq '.Trails[] | select(.name=="pwsh-cloud-trail").name') ) {
   Write-Host "CloudTrail still exists."
 }
 
 <# Check if tfstate bucket exists #>
-if ( $(aws s3api list-buckets --query "Buckets[].Name" | jq -r '.[] | select(startswith("pwsh-tfstate"))') -ne $null ) {
+if ( $null -ne $(aws s3api list-buckets --query "Buckets[].Name" | jq -r '.[] | select(startswith("pwsh-tfstate"))') ) {
   Write-Host "tfstate S3 bucket still exists."
 }
 
 <# Check if deployment bucket exists #>
-if ( $(aws s3api list-buckets --query "Buckets[].Name" | jq -r '.[] | select(startswith("deployment"))') -ne $null ) {
+if ( $null -ne $(aws s3api list-buckets --query "Buckets[].Name" | jq -r '.[] | select(startswith("deployment"))') ) {
   Write-Host "deployment S3 bucket still exists."
 }
 
 <# Check if EC2 instance exists #>
-if ( $(aws ec2 describe-instances --filters Name=tag-value,Values=bwsrv --query Reservations[0]) -ne "null" ) {
+if ( "null" -ne $(aws ec2 describe-instances --filters Name=tag-value,Values=bwsrv --query Reservations[0]) ) {
   Write-Host "EC2 instance exists."
 }
